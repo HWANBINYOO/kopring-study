237550b35612e64f6ce5b68e25ef7c512501530b
HWANBINYOO <dngh0825@naver.com>
2025-01-19 20:13:07 +0900

    inital

--- src/main/kotlin/com/training/backend/controller/CommentController.kt
+++
@@ -1,4 +0,0 @@
-package com.training.backend.controller
-
-class CommentController {
-}
--- src/main/kotlin/com/training/backend/controller/Controller.kt
+++ src/main/kotlin/com/training/backend/controller/Controller.kt
@@ -2,11 +2,8 @@ package com.training.backend.controller
 
 import com.training.backend.service.BoardService
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
